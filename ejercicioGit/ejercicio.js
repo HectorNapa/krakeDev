200388052786e2db33fb0724f113adc74cf379f3
HectorNapa <hectorsantosnapa@gmail.com>
2023-08-15 16:20:39 -0500

    el boton modificar la imagen

--- ejercicioGit/ejercicio.js
+++ ejercicioGit/ejercicio.js
@@ -1,3 +1,6 @@
 saludar= function(){
+    let cmpImagenSaludo;
+    cmpImagenSaludo= document.getElementById('imgSaludo');
+    cmpImagenSaludo.src='./imagenes/imagen.gif';
     console.log('ingresar a saludar');
 }
