e5b82c775094c861bb8e25f0c7be2b453cddaf47
HectorNapa <hectorsantosnapa@gmail.com>
2023-08-16 13:35:52 -0500

    Funcion mostrar texto implementado

--- utilitarios/saludo.js
+++ utilitarios/saludo.js
@@ -5,6 +5,8 @@ saludar= function(){
     let apellido= recuperarTexto('txtApellido');
     let edad = recuperarInt('txtEdad'); 
     let estatura= recuperarFloat('txtEstatura');
+    let mensajeBienvenida='Bienvenido '+nombre+' '+apellido;
+    mostrarTexto('lblResultado', mensajeBienvenida );
 }
 recuperarTexto= function(idComponente){
     let componente;
@@ -22,4 +24,9 @@ recuperarFloat= function(idComponente){
     let valorCaja= recuperarTexto(idComponente);
     let valorFlotante= parseFloat(valorCaja);
     return valorFlotante;
+}
+mostrarTexto= function(idComponente, mensaje){
+    let componente;
+    componente= document.getElementById(idComponente);
+    componente.innerText= mensaje;
 }
