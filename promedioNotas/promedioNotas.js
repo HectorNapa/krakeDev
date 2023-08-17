b303b5afb1c25c66c0babe25b9cf5693abae51a7
HectorNapa <hectorsantosnapa@gmail.com>
2023-08-17 16:13:46 -0500

    Se agrego condicional para evaluar notas

--- promedioNotas/promedioNotas.js
+++ promedioNotas/promedioNotas.js
@@ -5,4 +5,9 @@ calcularPromedioNotas= function(){
     let promedio= calcularPromedio(priNota, segNOta, terNota);
     let resultado= promedio.toFixed(2);
     mostrarTexto('lblPromedio','Su promedio es: '+ resultado);
+    if(promedio > 7){
+        mostrarImagen('imgImagen','./Imagenes/exito.gif');
+    }else{
+        mostrarImagen('imgImagen','./Imagenes/fracaso.gif')
+    }
 }
