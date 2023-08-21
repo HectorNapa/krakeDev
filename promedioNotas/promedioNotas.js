6422ae0784bef50fceca359d49f74f69d6b33e44
HectorNapa <hectorsantosnapa@gmail.com>
2023-08-21 18:40:40 -0500

    Condicionales para evalucion de promedios etre el 0 al 10

--- promedioNotas/promedioNotas.js
+++ promedioNotas/promedioNotas.js
@@ -5,9 +5,18 @@ calcularPromedioNotas= function(){
     let promedio= calcularPromedio(priNota, segNOta, terNota);
     let resultado= promedio.toFixed(2);
     mostrarTexto('lblPromedio','Su promedio es: '+ resultado);
-    if(promedio > 7){
-        mostrarImagen('imgImagen','./Imagenes/exito.gif');
+    if(promedio < 5 && promedio > 0 ){
+        mostrarImagen('imgImagen','./Imagenes/fracaso.gif');
+        mostrarTexto('lblResultado','REPROBADO !!!')
+    }else if(promedio >= 5 && promedio <= 8 ){
+        mostrarImagen('imgImagen','./Imagenes/exito.gif')
+        mostrarTexto('lblResultado','BUEN TRABAJO!!!')
+    }else if(promedio > 8 && promedio <=10 ){
+        mostrarImagen('imgImagen','./Imagenes/exelente.gif')
+        mostrarTexto('lblResultado','EXELENTE!!!')
     }else{
-        mostrarImagen('imgImagen','./Imagenes/fracaso.gif')
+        mostrarImagen('imgImagen','./Imagenes/incorrecto.gif')
+        mostrarTexto('lblResultado','DATOS INCORRECTOS!!!')
     }
+    
 }
