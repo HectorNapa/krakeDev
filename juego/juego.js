9ea228d4ce33b3363d1d0c70f8676af6a5ee9552
HectorNapa <hectorsantosnapa@gmail.com>
2023-08-25 15:18:40 -0500

    Implementacion y muestra de pantalla del puntaje de usuario y computador

--- juego/juego.js
+++ juego/juego.js
@@ -1,14 +1,21 @@
+let puntoUsuarios= 0;
+let puntoComputadora= 0;
 jugar= function(seleccionado){
+    let puntosMensaje;
     let elemento= generarElemento();
     let ruta= generarRuta(elemento);
     mostrarImagen('imgPiePapTije', ruta);
-    
+
     let ganador= determinarGanador(seleccionado,elemento);
     if(ganador == '0'){
         mostrarTexto('mensajeGanar','EMPATE');
    }else if(ganador == '1'){
+        puntoUsuarios= puntoUsuarios + 1;
+        mostrarTexto('puntoUsuario',puntoUsuarios);
         mostrarTexto('mensajeGanar','GANASTE LA PARTIDA!!');
    }else if(ganador == '2'){
-        mostrarTexto('mensajeGanar','PERDISTE LA PARTIDA!!');
+          puntoComputadora= puntoComputadora + 1;
+          mostrarTexto('puntoComputa', puntoComputadora);
+          mostrarTexto('mensajeGanar','PERDISTE LA PARTIDA!!');
    }
 }
