8e34f4736b233bfff0a549afb75dc457079d1a9c
HectorNapa <hectorsantosnapa@gmail.com>
2023-08-29 16:28:35 -0500

    Implementacion del PASO 1 ingreso de palabra secreta

--- JuegoDelAhorcado/ahorcado.js
+++ JuegoDelAhorcado/ahorcado.js
@@ -1,4 +1,26 @@
 //No se olvide de respirar, mantenga la calma y demuestre lo que sabe
+let palabraSecreta='';
+guardarPalabra= function(){
+    let contMayuscula= 0 ;
+    let letra;
+    let palabra= recuperarTexto('txtSecreta');
+    let longPalabra= palabra.length;
+
+    if(longPalabra == 5){
+        for(let i=0 ; i < longPalabra ; i++){
+            letra= palabra.charAt(i);
+            if(esMayuscula(letra)){
+                contMayuscula++;
+            }
+        }
+        if(contMayuscula == 5){
+            palabraSecreta= palabra;
+            console.log(palabraSecreta);
+        }else{
+            alert('Ingresar una palabra de 5 letras mayuculas');
+        }
+}
+}
 esMayuscula= function(caracter){
     let valida;
     let letra= caracter.charCodeAt(0);
