95609dcc204e3ab24ada9bb4e4d7209d8bdd1543
HectorNapa <hectorsantosnapa@gmail.com>
2023-08-30 17:45:38 -0500

    Version inicial arreglo, mostrar numeros aleatorio en tabla el tamaño generado por el usuario

--- arreglos/ejercicios1.js
+++ arreglos/ejercicios1.js
@@ -12,6 +12,7 @@ probarAgregar= function(){
 }
 agregarNota= function(nota){
     notas.push(nota);
+    mostrarNotas();
 }
 recorrerArreglo= function(){
     let notasR
@@ -25,6 +26,19 @@ generarTabla=function(){
     let cmpTabla= document.getElementById('divTabla');
     contenidoTabla += '<table><tr><td>UNO</td></tr>'+'<tr><td>DOS</td></tr></tabble>';
     cmpTabla.innerHTML=contenidoTabla;
+}
+mostrarNotas= function(){
+    let cmpTabla= document.getElementById('divTabla');
+    let  contenidoTabla='<table><tr><th>NOTA<th></tr>';
+    let miNota;
+    for(let i=0; i < notas.length; i++){
+        miNota= notas[i];
+        contenidoTabla+='<tr><td>';
+        contenidoTabla+= miNota;
+        contenidoTabla+='</tr></td>';
+    }
+    contenidoTabla+= '</table>';
+    cmpTabla.innerHTML= contenidoTabla;
 }
 calcularPromedio= function(){
     let sumaNotas= 0;
