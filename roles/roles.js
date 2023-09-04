240a39467b77f8cc20cbf53df6278867afbca691
HectorNapa <hectorsantosnapa@gmail.com>
2023-09-04 11:21:59 -0500

    deshabilitar y habilitar componentes y validar registro nuevo o ya existente

--- roles/roles.js
+++ roles/roles.js
@@ -1,6 +1,9 @@
+//Variables global
+let esNuevo= false;
 let empleados = [
     {cedula:"1714616123",nombre:"John",apellido:"Cena",sueldo:500.0},
-    {cedula:"0914632123",nombre:"Luisa",apellido:"Gonzalez",sueldo:900.0}
+    {cedula:"0914632123",nombre:"Luisa",apellido:"Gonzalez",sueldo:900.0},
+    {cedula:"0823445361",nombre:"Alberto",apellido:"Mendoza", sueldo:800.0}
 ]
 //Mostrar divEmpleado y ocultar divRol y divResumen
 mostrarOpcionEmpleado= function(){
@@ -8,6 +11,12 @@ mostrarOpcionEmpleado= function(){
     ocultarComponente('divResumen');
     mostrarComponente('divEmpleado');
     mostrarEmpleados();
+    //Deshabilitar componentes
+    deshabilitarComponente('txtCedula');
+    deshabilitarComponente('txtNombre');
+    deshabilitarComponente('txtApellido');
+    deshabilitarComponente('txtSueldo');
+    deshabilitarComponente('btnGuardar');
 }
 //Mostrar divRol  y ocultar  y divEmpleado divResumen
 mostrarOpcionRol= function(){
@@ -41,4 +50,22 @@ mostrarEmpleados= function(){
     }
     contenidoTabla+= '</table>';
     cmpTablaEmpleado.innerHTML= contenidoTabla;
+}
+ejecutarNuevo= function(){
+    //Habilita los componentes
+    habilitarComponente('txtCedula');
+    habilitarComponente('txtNombre');
+    habilitarComponente('txtApellido');
+    habilitarComponente('txtSueldo');
+    habilitarComponente('btnGuardar');
+    let cedula= recuperarTexto('txtCedula');
+    let empleado;
+    for(let i=0; i<empleados.length; i++){
+        empleado= empleados[i];
+        if(empleado.cedula == cedula){
+            esNuevo= false;
+            break;
+        }
+    }
+    esNuevo= true;
 }
